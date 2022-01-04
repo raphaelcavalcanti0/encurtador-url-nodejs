3e8955c3f5401707d2027104ff77bcbd372ff5ad
Raphael Cavalcanti <46363242+raphaelcavalcanti0@users.noreply.github.com>
2022-01-04 07:26:48 -0300

    remove db string connection

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:4000',
-    MONGO_CONNECTION: 'mongodb+srv://raphaelcavalcantizero:078197@cluster0.elvnk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MONGO_CONNECTION: ''
 }
